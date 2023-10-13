efbca2f5e82c323931897e4afd82df6d41843b05
VipulVW14 <vipulwasnik0@gmail.com>
2023-10-13 20:37:53 +0530

    tailwind issue resolved

--- apps/client/postcss.config.js
+++ apps/client/postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+module.exports = require("@vipul/configs/tailwind/postcss.config");
