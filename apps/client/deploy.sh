b27e3d1a3e9e188beefffc1009941209454f33a2
VipulVW14 <vipulwasnik0@gmail.com>
2023-11-18 18:23:56 +0530

    update ci/cd

--- apps/client/deploy.sh
+++ apps/client/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export PATH=$PATH:home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
-cd /home/ubuntu/coursewave
+cd /Coursewave/apps/client/
 git pull origin main
 yarn install 
 yarn build 
 pm2 stop next
 pm2 start npm --name "next" --run "start:next"
+
