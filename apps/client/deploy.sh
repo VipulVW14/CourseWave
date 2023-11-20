e55ddfe02495694e7c7822f31575a46855869d19
VipulVW14 <vipulwasnik0@gmail.com>
2023-11-21 00:11:42 +0530

    update client deploy.sh

--- apps/client/deploy.sh
+++ apps/client/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export PATH=$PATH:home/ubuntu/.nvm/versions/node/v21.2.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.2.0/bin
 
 cd /home/ubuntu/CourseWave/ 
 git pull origin main
-/home/ubuntu/.nvm/versions/node/v21.2.0/bin/yarn install 
-/home/ubuntu/.nvm/versions/node/v21.2.0/bin/yarn build 
-/home/ubuntu/.nvm/versions/node/v21.2.0/bin/pm2 stop next
-/home/ubuntu/.nvm/versions/node/v21.2.0/bin/pm2 start npm --name "next" -- run "start:next"
+yarn install 
+yarn build 
+pm2 stop next
+pm2 start npm --name "next" -- run "start:next"
 
  
