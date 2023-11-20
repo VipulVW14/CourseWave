cd7b44ef24324268d9d857a805430efff0de15e3
VipulVW14 <vipulwasnik0@gmail.com>
2023-11-21 00:35:17 +0530

    update client deploy.sh

--- apps/client/deploy.sh
+++ apps/client/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.2.0/bin
 
+. /home/ubuntu/.nvm/nvm.sh
+nvm use 21.2.0
+
+echo "Current PATH: $PATH"
+node -v
+
 cd /home/ubuntu/CourseWave/ 
 git pull origin main
 yarn install 
