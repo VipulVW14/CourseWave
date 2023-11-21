79441cf58ac23b6ec90d64ad13341e5dae45a2d7
VipulVW14 <vipulwasnik0@gmail.com>
2023-11-21 16:37:54 +0530

    code cleanup

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "ui/styles.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react"
 import { RecoilRoot } from 'recoil';
-import { InitUser } from "@/components/InitUser";
 import Appbar from "@/components/Appbar";
 import dynamic from "next/dynamic";
 
@@ -10,7 +9,6 @@ function MyApp({ Component,  pageProps: { session, ...pageProps }, }: AppProps) 
   return(
     <SessionProvider session={pageProps.session}>
       <RecoilRoot>
-        {/* <InitUser/> */}
         <Appbar/>
         <Component {...pageProps} />
       </RecoilRoot>
